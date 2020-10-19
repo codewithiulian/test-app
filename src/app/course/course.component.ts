ebb2d55ff58c814bbbcf71b3dfe203b284260b77
Iulian Oana <iulian.andreioana@gmail.com>
2020-10-19 18:44:57 +0100

    Added the types of bindings covered so far.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -10,6 +10,7 @@ export class CourseComponent implements OnInit {
   title = "List of courses";
   courses;
   isActive = true;
+  imageUrl = "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
 
   constructor(service: CourseService) {
     this.courses = service.getCourses();
