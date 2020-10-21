4fe3a2a3bd6e12dc915507412750f8453640667f
Iulian Oana <iulian.oana@cemar.co.uk>
2020-10-21 16:15:07 +0100

    76. ngModel

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ContactFormComponent {
+  log(e){console.log(e);}
 }
