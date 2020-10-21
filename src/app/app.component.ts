9a57597dec002e6821ad174aa3d4357864c38b1b
Iulian Oana <iulian.oana@cemar.co.uk>
2020-10-21 11:31:44 +0100

    71. Safe Traversal Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TwitterModel } from './models/TwitterModel';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,10 @@ import { TwitterModel } from './models/TwitterModel';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  canSave = true;
+  task = {
+    title: 'Review applications',
+    assignee: {
+      name: 'John Smith'
+    }
+  }
 }
