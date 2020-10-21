522d3542767f0027c067b8da835ab74a49a6cbd6
Iulian Oana <iulian.oana@cemar.co.uk>
2020-10-21 11:28:39 +0100

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,5 @@ import { TwitterModel } from './models/TwitterModel';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses;
-
-
-  loadCourses(){
-    this.courses = [
-      { id: 1, name: 'Course 1' },
-      { id: 2, name: 'Course 2' },
-      { id: 3, name: 'Course 3' },
-    ];
-  }
-
-  trackCourse(index, course){
-    return course ? course.id : undefined
-  }
+  canSave = true;
 }
