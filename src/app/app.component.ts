ce12be6df8ec2a6487c4614455da755575d9139b
Iulian Oana <iulian.oana@cemar.co.uk>
2020-10-21 11:15:47 +0100

    ngFor and trackBy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,18 @@ import { TwitterModel } from './models/TwitterModel';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses = [
-    { id: 1, name: 'Course 1' },
-    { id: 2, name: 'Course 2' },
-    { id: 3, name: 'Course 3' },
-  ];
+  courses;
 
-  onAdd(){
-    let id = this.courses[this.courses.length-1].id + 1;
-    this.courses.push({ id: id, name: `Course ${id}` });
+
+  loadCourses(){
+    this.courses = [
+      { id: 1, name: 'Course 1' },
+      { id: 2, name: 'Course 2' },
+      { id: 3, name: 'Course 3' },
+    ];
   }
 
-  onRemove(course){
-    this.courses.splice(this.courses.indexOf(course), 1);
+  trackCourse(index, course){
+    return course ? course.id : undefined
   }
 }
